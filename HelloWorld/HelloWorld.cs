47a72e76a7492b65a681a7120502b32e68183715
ChaseWoods89 <chasewoods89@gmail.com>
2019-04-02 19:09:22 -0500

    changed back to normal

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Chase!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
