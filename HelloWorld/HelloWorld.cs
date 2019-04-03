c0b75879c34607c210ddbc284d4014cd43337e22
ChaseWoods89 <chasewoods89@gmail.com>
2019-04-02 21:04:15 -0500

    Everything but 100/10 bit done

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -27,7 +27,30 @@ namespace HelloWorld
             int Yards = Convert.ToInt32(yardsInput);
             int Inches = Yards*36;
 
-            Console.WriteLine("There are " + Inches + " inches in " + Yards + "."); 
+            Console.WriteLine("There are " + Inches + " inches in " + Yards + ".");
+
+            Console.WriteLine("--------------------------------------");
+
+
+            var people = true;
+            var f = false;
+            double num = 2.5;
+
+            Console.WriteLine( num + " times itself is " + num*num);
+
+            Console.WriteLine("--------------------------------------");
+
+            string firstName = "Chase";
+            string lastName = "Woods";
+            int age = 29;
+            string job = "Lyft Driver";
+            string favoriteBand = "Animals As Leaders";
+            string favoriteSportsTeam = " the Tampa Bay Lightning, Dallas Cowboys, Kentucky Wildcats in no particular order.";
+
+            Console.WriteLine("Hey there! My name is " + firstName + " " + lastName + ". I am " + age + " years old and I am a " + job + ". My favorite band right now is " + favoriteBand + ", they are awesome and you should check them out. My favorite sports teams are " + favoriteSportsTeam + ", The Lightning are totally winning the Stanley Cup! Go Bolts!");
+
+            Console.WriteLine("--------------------------------------");
+
 
 
 
