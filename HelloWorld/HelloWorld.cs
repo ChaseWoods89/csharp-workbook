f37068cd3935a65f7c7b0992fd09c167818c4631
ChaseWoods89 <chasewoods89@gmail.com>
2019-04-02 20:25:18 -0500

    First part of Hello World, before entire assignment

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -6,7 +6,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hey there stranger, what's your name?");
+            String userName = Console.ReadLine();
+            Console.WriteLine("Well, pleasure to meet you " + userName + ". My Name is Chase!");
+            Console.ReadLine();
         }
     }
 }
