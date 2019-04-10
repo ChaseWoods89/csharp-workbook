4d72f933ecd194f0f20aae9572985aa03658d0d5
ChaseWoods89 <chasewoods89@gmail.com>
2019-04-09 20:58:07 -0500

    RPS game completed except for score keeping

--- RockPaperScissors/RockPaperScissors.cs
+++ RockPaperScissors/RockPaperScissors.cs
@@ -4,23 +4,27 @@ namespace RockPaperScissors
 {
     class Program
     {
-        public static void Main()
-        {
+        public static void Main(){
+
             Console.WriteLine("Enter hand 1:");
             string hand1 = Console.ReadLine().ToLower();
-            Console.WriteLine("Enter hand 2:");
-            string hand2 = Console.ReadLine().ToLower();
+            Random generator = new Random();
+            string [] hand2Choice = {"rock", "paper", "scissors"};
+            int hand2Index = generator.Next(0, hand2Choice.Length);
+            string hand2 = hand2Choice[hand2Index];
+            Console.WriteLine("Hand 2 is " + hand2);
             Console.WriteLine(CompareHands(hand1, hand2));
 
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
+            
         }
         
         public static string CompareHands(string hand1, string hand2)
         {
              if (hand1 == hand2){
-              return "It's a tie.";}
-        
+              return "It's a tie.";
+              }
 
             else if (hand1 == "rock" && hand2 == "scissors"){
               return "Hand 1 Wins!!!";
