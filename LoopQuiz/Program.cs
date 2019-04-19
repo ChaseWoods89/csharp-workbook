88961347de88e7ab7f26c2807c92ed058eae4393
ChaseWoods89 <chasewoods89@gmail.com>
2019-04-18 20:06:33 -0500

    Work in class

---
+++ LoopQuiz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LoopQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers = new int[] {5, 12, 4, 10, 22};
+            int biggestOneSoFar = Numbers[0];
+            for (int idx = 0; idx < Numbers.Length; idx++ )
+            {
+                int currentNumber = Numbers[idx];
+                if( biggestOneSoFar < currentNumber)
+                {
+                    biggestOneSoFar = currentNumber;
+                
+                }
+            
+            }
+            Console.WriteLine(biggestOneSoFar);
+        }
+    }
+}
