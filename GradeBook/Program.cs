1b8fff8b46b3480319f8cdfaccb598420ac4b474
ChaseWoods89 <chasewoods89@gmail.com>
2019-04-18 20:56:25 -0500

    can print the looped dictionary

---
+++ GradeBook/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,string> gradeBook = new Dictionary <string, string>();
+            int i = 0;
+            for( i = 0; i < 5; i++){
+                Console.WriteLine("Give me the student's name.");
+                string student = Console.ReadLine();
+                Console.WriteLine("What kind of grades does student have? (please enter 4 grades 0-100)");
+                gradeBook.Add(student,Console.ReadLine());
+            }
+
+            foreach(var student in gradeBook){
+                Console.WriteLine(student);
+            }
+
+
+
+
+           Console.ReadLine();
+        }
+    }
+}
