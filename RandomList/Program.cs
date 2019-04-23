45c18563568ec4e05f5345d5d3b6151280d8fceb
ChaseWoods89 <chasewoods89@gmail.com>
2019-04-23 18:49:02 -0500

    in class work

---
+++ RandomList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            List<string> students = new List<string>{};
+            students.Add("Hank");
+            students.Add("Peggy");
+            students.Add("Bobby");
+            students.Add("LadyBird");
+            students.Add("Dale");
+            students.Add("Khan");
+            students.Add("Bill");
+
+            Random randomName = new Random();
+            
+
+            foreach(string person in students){
+                Console.WriteLine(randomName.Next(0,6));
+            }
+
+            
+            
+        }
+    }
+}
