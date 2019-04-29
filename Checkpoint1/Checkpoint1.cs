9430eeb53c66d3896e11fb2fe5106004472df2ca
ChaseWoods89 <chasewoods89@gmail.com>
2019-04-29 13:01:18 -0500

    finished program 1

--- Checkpoint1/Checkpoint1.cs
+++ Checkpoint1/Checkpoint1.cs
@@ -6,7 +6,24 @@ namespace Checkpoint1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~Program 1~~~~~~~~~~~~~~~~~~");  
+            int divisibleNumber = 0;
+            for(int i = 1; i < 101; i++){
+                if (i%3 == 0){
+                    divisibleNumber++;
+                }
+                }
+            Console.WriteLine(divisibleNumber + "Is the number of times 3 goes into 100 with no remainder.");   
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Press enter to start program 2!");    
+            Console.ReadLine();
+                        
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~Program 2~~~~~~~~~~~~~~~~~~");  
+
+        
+        
+        
         }
     }
 }
