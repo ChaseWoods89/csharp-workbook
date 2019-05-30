573bf139b7481f1052bfc4ce30bddb549a6c5b2c
ChaseWoods89 <chasewoods89@gmail.com>
2019-05-30 13:57:18 -0500

    I think my work is done for homework.

--- week9-1/Program.cs
+++ week9-1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace week9_1
 {
@@ -8,9 +9,28 @@ namespace week9_1
         static void Main(string[] args)
         {
             DAO dao = new DAO();
-            dao.create("Lord of the Rings", Type.Fiction);
-            dao.create("Hop on Pop", Type.Kids);
-            dao.create("Theory of Everything", Type.Biography);
+            bool exit = false;
+            while (exit == false){
+
+            Console.WriteLine("Please enter a book title or enter exit to finish.");
+            string userInput1 = Console.ReadLine();
+
+            if (userInput1 == "exit" || userInput1 == "Exit") {
+                exit = true;
+            }
+            Console.WriteLine("What Genre is the book? (Kids, Fiction, Biography)");
+            string userInput2 = Console.ReadLine().ToLower();
+
+            if (userInput2 == "kids") {
+                dao.create(userInput1, Type.Kids);
+            }
+            else if (userInput2 == "fiction"){
+                dao.create(userInput1, Type.Fiction);
+            }
+            else if (userInput2 == "biography"){
+                dao.create(userInput1, Type.Fiction);
+            }
+            }
 
             foreach (Book b in dao.listBooks())
             {
@@ -21,10 +41,12 @@ namespace week9_1
 
     public class Book 
     {
-        public int id;
-        public string name;
-        public Type type;
-
+        public int id {get; private set;}
+        public string name {get; private set;}
+        public Type type {get; private set;}
+        public Book()
+        {        
+        }
         public Book( string intialName, Type initialType){
             this.name = intialName;
             this.type = initialType;
@@ -39,23 +61,25 @@ namespace week9_1
     public class DAO
     {
         //this class manages the data access to books
-        private List<Book> books;
+        private BooksContext context;
         //creates a new instance of dao
         public DAO()
         {
-            books = new List<Book>();
+            context = new BooksContext();
+            context.Database.EnsureCreated();
         }
         //create coresponding book and store it.
         public void create(string name, Type type)
         {
             Book newBook = new Book(name, type);
-            books.Add(newBook);
+            context.books.Add(newBook);
+            context.SaveChanges();
         }
         //return the current list of books
         public List<Book> listBooks()
         {
              List<Book> result = new List<Book>();
-             foreach (Book b in books){
+             foreach (Book b in context.books){
                  result.Add(b);
                  }
                  return result;
@@ -64,7 +88,7 @@ namespace week9_1
         public List<Book> listFictionBooks()
         {
             List<Book> fictionBooks = new List<Book>();
-            foreach(Book b in books){
+            foreach(Book b in context.books){
                 if (b.type == Type.Fiction){
                     fictionBooks.Add(b);
                 }
@@ -72,5 +96,14 @@ namespace week9_1
 
                 return fictionBooks;
             }
-        }
+
+            public class BooksContext : DbContext
+            {
+                public DbSet<Book> books {get; private set;}
+                protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+                {
+                    optionsBuilder.UseSqlite("Filename=./books.db");
+                }
+            }        
+            }
     }
